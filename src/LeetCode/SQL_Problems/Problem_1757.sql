ddb47c669d7c872333e8f2c80664790b112135d0
Izabela Łukawska <lukawska.cf@gmail.com>
2024-11-12 16:56:01 +0100

    solution for Leet Code 1757

---
+++ src/LeetCode/SQL_Problems/Problem_1757.sql
@@ -0,0 +1,2 @@
+--LINK: https://leetcode.com/problems/recyclable-and-low-fat-products/?envType=study-plan-v2&envId=top-sql-50
+SELECT (product_id) from Products WHERE low_fats = 'Y' AND recyclable = 'Y';
