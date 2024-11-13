e542fbf1b0885593b9bd26a13938216d9669700a
Izabela Łukawska <lukawska.cf@gmail.com>
2024-11-13 18:23:39 +0100

    added ordering by id

--- src/LeetCode/SQL_Problems/Easy/Problem_1148.sql
+++ src/LeetCode/SQL_Problems/Easy/Problem_1148.sql
@@ -1,2 +1,2 @@
 --LINK : https://leetcode.com/problems/article-views-i/description/?envType=study-plan-v2&envId=top-sql-50
-SELECT DISTINCT author_id AS id FROM Views WHERE author_id = viewer_id;
+SELECT DISTINCT author_id AS id FROM Views WHERE author_id = viewer_id ORDER BY id;
