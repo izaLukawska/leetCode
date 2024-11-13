c3a20ff2ca9d6055c3e59eef31a85251b3efe91b
Izabela Łukawska <lukawska.cf@gmail.com>
2024-11-13 18:33:12 +0100

    code refactor

--- src/LeetCode/SQL_Problems/Problem_1683.sql
+++ src/LeetCode/SQL_Problems/Problem_1683.sql
@@ -1,2 +1,2 @@
 --LINK: https://leetcode.com/problems/invalid-tweets/description/?envType=study-plan-v2&envId=top-sql-50
-SELECT tweet_id from Tweets WHERE LENGTH(content) > 15;
+SELECT tweet_id FROM Tweets WHERE LENGTH(content) > 15;
