8a63016dca0327d1dfc8b8523cc61c0136373859
Izabela Łukawska <lukawska.cf@gmail.com>
2024-11-15 19:21:43 +0100

    solution for LeetCode 24

---
+++ src/LeetCode/Java_Problems/LeetCode_24.java
@@ -0,0 +1,37 @@
+package LeetCode.Java_Problems;
+
+//LINK: https://leetcode.com/problems/swap-nodes-in-pairs/description/
+
+public class LeetCode_24 {
+	public ListNode swapPairs(ListNode head) {
+		ListNode result = new ListNode(0);
+		result.next = head;
+		ListNode point = result;
+
+		while(point.next != null && point.next.next != null){
+			ListNode swap1 = point.next;
+			ListNode swap2 = point.next.next;
+
+			swap1.next = swap2.next;
+			swap2.next = swap1;
+
+			point.next = swap2;
+			point = swap1;
+		}
+		return result.next;
+	}
+	public static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {}
+
+		ListNode(int val) {this.val = val;}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+}
