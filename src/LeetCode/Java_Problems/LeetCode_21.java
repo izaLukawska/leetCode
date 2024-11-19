f040633935df7311c4c704779cb0504c8deb2439
Izabela Łukawska <lukawska.cf@gmail.com>
2024-11-19 19:18:37 +0100

    code refactor

--- src/LeetCode/Java_Problems/LeetCode_21.java
+++ src/LeetCode/Java_Problems/LeetCode_21.java
@@ -11,20 +11,23 @@ public class LeetCode_21 {
 				l3.next = l1;
 				l1 = l1.next;
 			}
-			else{
+			else {
 				l3.next = l2;
 				l2 = l2.next;
 			}
 			l3 = l3.next;
 		}
+
 		if(l1 != null){
 			l3.next = l1;
 		}
 		if(l2 != null){
 			l3.next = l2;
 		}
+
 		return result.next;
 	}
+
 	private static class ListNode {
 		int val;
 		ListNode next;
