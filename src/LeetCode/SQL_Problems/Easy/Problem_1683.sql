fc9011962d344ace4feb1e3ad7e8a651a6856212
Izabela Łukawska <lukawska.cf@gmail.com>
2024-11-24 17:22:35 +0100

    solution for LeetCode 1667

--- src/LeetCode/SQL_Problems/Easy/Problem_1683.sql
+++
@@ -1,2 +0,0 @@
---LINK: https://leetcode.com/problems/invalid-tweets/description/?envType=study-plan-v2&envId=top-sql-50
-SELECT tweet_id FROM Tweets WHERE LENGTH(content) > 15;
--- src/LeetCode/SQL_Problems/Easy/Problem_595.sql
+++
@@ -1,3 +0,0 @@
---LINK : https://leetcode.com/problems/big-countries/description/?envType=study-plan-v2&envId=top-sql-50
---code is not working if I put name, population, area with parentheses
-SELECT name, population, area FROM World WHERE (area >= 3000000) OR (population >= 25000000);
