c06b91691e3a3e81efc908d51ed9553056f79d08
Izabela Łukawska <lukawska.cf@gmail.com>
2024-11-24 12:26:15 +0100

    code refactor

--- src/LeetCode/Java_Problems/LeetCode_15.java
+++ src/LeetCode/Java_Problems/LeetCode_15.java
@@ -11,22 +11,27 @@ public class LeetCode_15 {
 	public List<List<Integer>> threeSum(int[] nums) {
 		Arrays.sort(nums);
 		Set<List<Integer>> result = new HashSet<>();
-		for(int i = 0 ; i < nums.length - 2; i++){
-			int leftPointer = i + 1;
-			int rightPointer = nums.length - 1;
-			while(leftPointer < rightPointer){
-				int sum = nums[i] + nums[leftPointer] + nums[rightPointer];
-				if(sum == 0){
-					result.add(Arrays.asList(nums[i], nums[leftPointer], nums[rightPointer]));
-					leftPointer++;
-					rightPointer--;
-				}
-				else if (sum < 0) {
-					leftPointer++;
-				}
-				else rightPointer--;
+		for (int i = 0; i < nums.length - 2; i++) {
+			checkForThreeSum(nums, i, result);
+		}
+
+		return result.stream().toList();
+	}
+
+	private static void checkForThreeSum(int[] nums, int index, Set<List<Integer>> result) {
+		int leftPointer = index + 1;
+		int rightPointer = nums.length - 1;
+		while (leftPointer < rightPointer) {
+			int sum = nums[index] + nums[leftPointer] + nums[rightPointer];
+			if (sum == 0) {
+				result.add(Arrays.asList(nums[index], nums[leftPointer], nums[rightPointer]));
+				leftPointer++;
+				rightPointer--;
+			} else if (sum < 0) {
+				leftPointer++;
+			} else {
+				rightPointer--;
 			}
 		}
-		return result.stream().toList();
 	}
 }
