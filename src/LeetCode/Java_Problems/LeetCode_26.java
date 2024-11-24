72f2db4424efeb259227ba05fd4af86dd3a9f3ac
Izabela Łukawska <lukawska.cf@gmail.com>
2024-11-24 11:44:54 +0100

    solution for Leet Code 26

---
+++ src/LeetCode/Java_Problems/LeetCode_26.java
@@ -0,0 +1,16 @@
+package LeetCode.Java_Problems;
+
+//LINK: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+public class LeetCode_26 {
+	public int removeDuplicates(int[] nums) {
+		int count = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[count] != nums[i]) {
+				count++;
+				nums[count] = nums[i];
+			}
+		}
+		return count + 1;
+	}
+}
