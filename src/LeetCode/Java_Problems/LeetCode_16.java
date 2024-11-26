fa6e332d102305cf10b53dc94c2c020d8ebfa074
Izabela Łukawska <lukawska.cf@gmail.com>
2024-11-26 13:37:52 +0100

    code refactor

--- src/LeetCode/Java_Problems/LeetCode_16.java
+++ src/LeetCode/Java_Problems/LeetCode_16.java
@@ -1,35 +1,34 @@
 package LeetCode.Java_Problems;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 //LINK: https://leetcode.com/problems/3sum-closest/submissions/1453668768/
 
 public class LeetCode_16 {
 	public int threeSumClosest(int[] nums, int target) {
 		Arrays.sort(nums);
-		int sum = 0;
-		int difference = Integer.MAX_VALUE;
-		for(int i = 0; i < nums.length - 2; i++){
-			int leftPointer = i + 1;
-			int rightPointer = nums.length - 1;
-			while(leftPointer < rightPointer){
-				int currSum = nums[i] + nums[leftPointer] + nums[rightPointer];
-				if(currSum == target){
-					return target;
-				}
-				else if (currSum < target) {
-					leftPointer++;
-				}
-				else{
-					rightPointer--;
-				}
-				int currDiff = Math.abs(currSum - target);
-				if(currDiff < difference){
-					difference = currDiff;
-					sum = currSum;
-				}
+		Map<Integer, Integer> sumDiff = new HashMap<>();
+		for (int i = 0; i < nums.length - 2; i++) {
+			calculateSumDiff(nums, i, target, sumDiff);
+		}
+
+		return sumDiff.entrySet().stream().min(Map.Entry.comparingByValue()).get().getKey();
+	}
+
+	private static void calculateSumDiff(int[] nums, int index, int target, Map<Integer, Integer> sumDiff) {
+		int leftPoint = index + 1;
+		int rightPoint = nums.length - 1;
+
+		while (leftPoint < rightPoint) {
+			int sum = nums[index] + nums[leftPoint] + nums[rightPoint];
+			sumDiff.put(sum, Math.abs(sum - target));
+			if (sum < target) {
+				leftPoint++;
+			} else {
+				rightPoint--;
 			}
 		}
-		return sum;
 	}
 }
