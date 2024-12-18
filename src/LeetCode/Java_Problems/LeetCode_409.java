35fe4f92926e8feb89b0208d414e2fed6dd8f3d1
Izabela Łukawska <lukawska.cf@gmail.com>
2024-12-18 17:37:02 +0100

    Solution for LeetCode 409

---
+++ src/LeetCode/Java_Problems/LeetCode_409.java
@@ -0,0 +1,30 @@
+package LeetCode.Java_Problems;
+
+//LINK: https://leetcode.com/problems/longest-palindrome/description/
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class LeetCode_409 {
+	public static int longestPalindrome(String s) {
+		int len = 0;
+		boolean isOdd = false;
+		for (int count : charCount(s)) {
+			if (count % 2 == 0) {
+				len += count;
+			} else {
+				len += count - 1;
+				isOdd = true;
+			}
+		}
+		return isOdd ? len + 1 : len;
+	}
+
+	private static List<Integer> charCount(String s){
+		return Arrays.stream(s.split(""))
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+				.values().stream().mapToInt(Long::intValue).boxed().toList();
+	}
+}
