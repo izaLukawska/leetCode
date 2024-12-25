40805c78947511b275ca4511fffaaad3c3fac5d5
Izabela Łukawska <lukawska.cf@gmail.com>
2024-12-25 13:00:00 +0100

    solution for LeetCode_74

---
+++ src/LeetCode/Java_Problems/LeetCode_74.java
@@ -0,0 +1,16 @@
+package LeetCode.Java_Problems;
+
+//LINK: https://leetcode.com/problems/search-a-2d-matrix/
+
+public class LeetCode_74 {
+	public boolean searchMatrix(int[][] matrix, int target) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				if (matrix[i][j] == target) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
