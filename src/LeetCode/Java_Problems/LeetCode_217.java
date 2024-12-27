cbcffe31abc87e022aee4a9f4ebaefd904c288e6
Izabela Łukawska <lukawska.cf@gmail.com>
2024-12-27 16:46:35 +0100

    solution for LeetCode_217

---
+++ src/LeetCode/Java_Problems/LeetCode_217.java
@@ -0,0 +1,27 @@
+package LeetCode.Java_Problems;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+//LINK: https://leetcode.com/problems/contains-duplicate/
+
+public class LeetCode_217 {
+	public boolean containsDuplicate(int[] nums) {
+		Set<Integer> numsSet = new HashSet<>();
+		for (int num : nums) {
+			if (!numsSet.contains(num)) {
+				numsSet.add(num);
+			} else {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public boolean containsDuplicate2(int[] nums) {
+		return Arrays.stream(nums).boxed().collect(Collectors.toSet()).size() != nums.length;
+	}
+}
